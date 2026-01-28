1709787dda787ff71239cd3cf921183cab84351e
TurkerAlbayrak <turkeralb@gmail.com>
2026-01-28 17:24:43 +0300

    python-datatypes-objects/python_lists/lists_methods.py update

---
+++ python-datatypes-objects/python_lists/lists_methods.py
@@ -0,0 +1,11 @@
+numbers = [1,2,3]
+numbers.append(5)
+numbers.insert(3,70) # 3. indexe 70 ekle
+numbers.pop() #en son silinir
+numbers.pop(0) # ilk index silinir
+numbers.remove(2) # 2 yi sil
+numbers.sort() #sayısal olarak sıralanır
+numbers.reverse() # liste tam terse çevrirlir
+print(numbers.count(1)) # 1 den kaç tane var
+numbers.clear() # bütün elemanları siler
+
